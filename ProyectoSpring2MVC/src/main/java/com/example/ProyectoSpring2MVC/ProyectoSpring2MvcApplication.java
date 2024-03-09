d678b5fd94394549fb7f931d9035e6485bcc287a
Javier <franjavierros@gmail.com>
2024-03-09 19:42:34 +0100

    Proyecto2

---
+++ ProyectoSpring2MVC/src/main/java/com/example/ProyectoSpring2MVC/ProyectoSpring2MvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoSpring2MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpring2MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpring2MvcApplication.class, args);
+	}
+
+}
