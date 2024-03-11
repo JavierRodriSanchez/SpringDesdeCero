448ef84480df8ac26c642efc9cb3f1855b4e1847
Javier <franjavierros@gmail.com>
2024-03-11 23:13:33 +0100

    Enviando a la vista un json y una plantilla html

--- ProyectoSpring2MVC/src/main/java/com/example/ProyectoSpring2MVC/controllers/UserRestController.java
+++ ProyectoSpring2MVC/src/main/java/com/example/ProyectoSpring2MVC/controllers/UserRestController.java
@@ -3,19 +3,16 @@ package com.example.ProyectoSpring2MVC.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class UserRestController {
 
 	@GetMapping("/details")	
-	@ResponseBody
 	public Map<String, Object> details() {
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("title","Hola Mundo Spring Boot");
